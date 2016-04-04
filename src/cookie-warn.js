c3d4122449b9a37f85d1dbc53dd32a0e9abf78f4
Tamas Schalk <tamas@schalk.hu>
2016-04-04 23:36:54 +0200

    optimalization

--- src/cookie-warn.js
+++ src/cookie-warn.js
@@ -41,15 +41,47 @@
 
 (function (fn) {
 
-    var tag, text, button, link, info, expire, cookie;
+    // set or get cookie
+    var cookie = function (name, value, days) {
+
+        if (value === undefined) {
+
+            var i, x, y, cookies = document.cookie.split(";");
+
+            for (i = 0; i < cookies.length; i++) {
+                x = cookies[i].substr(0, cookies[i].indexOf("="));
+                y = cookies[i].substr(cookies[i].indexOf("=") + 1);
+                x = x.replace(/^\s+|\s+$/g, "");
+                if (x == name) {
+                    return y;
+                }
+            }
+
+        } else {
+
+            days = days ? days : 14;
+            var expire = new Date();
+            expire.setDate(expire.getDate() + days);
+            value = value + ((days == null) ? "" : "; expires=" + expire.toGMTString());
+            document.cookie = name + "=" + value;
+
+        }
+
+    };
+
+
+    // if cookie true return
+    if (cookie(fn)) {
+        return;
+    }
 
     // warning box close function
     window[fn] = {
 
-        close: function () {
+        close: function (expire) {
 
             // set the cookie
-            cookie(fn, true);
+            cookie(fn, true, expire);
 
             // remove warning box
             var obj = document.getElementById('cookieWarnBox');
@@ -62,44 +94,12 @@
 
     };
 
-    // set and get cookie function
-    cookie = function (name, value, days) {
-
-        var expire;
-
-        if (days == undefined) {
-            days = expire ? expire : 14;
-        }
-
-        // if value not undefined set the cookie else read
-        if (value !== undefined) {
-
-            expire = new Date();
-            expire.setDate(expire.getDate() + days);
-            value = escape(value) + ((days == null) ? "" : "; expires=" + expire.toGMTString());
-            document.cookie = name + "=" + value;
-
-        } else {
-
-            var i, x, y, cookies = document.cookie.split(";");
-
-            for (i = 0; i < cookies.length; i++) {
-                x = cookies[i].substr(0, cookies[i].indexOf("="));
-                y = cookies[i].substr(cookies[i].indexOf("=") + 1);
-                x = x.replace(/^\s+|\s+$/g, "");
-                if (x == name) {
-                    return unescape(y);
-                }
-            }
-
-        }
-
-    }
-
     document.addEventListener('DOMContentLoaded', function () {
 
+        var tag, text, button, link, info, expire, css, style, style1, style2, style3, body, wbox, more;
+
         // get parameters
-        tag = document.getElementById('cookieWarn')
+        tag = document.getElementById('cookieWarn');
         text = tag.getAttribute('data-text');
         button = tag.getAttribute('data-button');
         link = tag.getAttribute('data-link');
@@ -107,15 +107,10 @@
         expire = parseInt(tag.getAttribute('data-expire'));
         style = tag.getAttribute('data-style');
 
-        // if cookie true return
-        if (cookie(fn)) {
-            return;
-        }
-
-        var style1 = '#cookieWarnBox {position:fixed;line-height:32px; bottom:0;left:0;right:0;background-color:#212121;color:#f1f1f1;opacity:0.9;text-align:center;padding:15px;font-size:18px;}';
-        var style2 = '#cookieWarnBox span {text-transform:uppercase;cursor:pointer;background-color:#f1f1f1;color:#1188ff;padding:7px 14px;margin-left:10px;}';
-        var style3 = '#cookieWarnBox span:hover {background-color:#fefefe;} #cookieWarnBox a {text-decoration:none;color:#1188ff;}';
-        var css = {
+        style1 = '#cookieWarnBox {position:fixed;line-height:32px; bottom:0;left:0;right:0;background-color:#212121;color:#f1f1f1;opacity:0.9;text-align:center;padding:15px;font-size:18px;}';
+        style2 = '#cookieWarnBox span {text-transform:uppercase;cursor:pointer;background-color:#f1f1f1;color:#1188ff;padding:7px 14px;margin-left:10px;}';
+        style3 = '#cookieWarnBox span:hover {background-color:#fefefe;} #cookieWarnBox a {text-decoration:none;color:#1188ff;}';
+        css = {
 
             type: 'text/css', style: document.createElement('style'),
             content: style1 + style2 + style3 + style,
@@ -130,16 +125,12 @@
 
         css.append();
 
-        var body, wbox, more, style;
-
         // create warning box
         wbox = document.createElement('div');
-
-        // set warning box id and html
         wbox.setAttribute("id", "cookieWarnBox");
         wbox.setAttribute("style", style.box);
         more = (link && info) ? ' <a target="_blank" href="' + link + '">' + info + '</a> ' : '';
-        button = '<span id="cookieWarnClose" onclick="javascript: ' + fn + '.close();">' + button + '</span>';
+        button = '<span id="cookieWarnClose" onclick="' + fn + '.close(' + expire + ');">' + button + '</span>';
         wbox.innerHTML = '<div class="text">' + text + more + button + '</div>';
 
         // append to body
