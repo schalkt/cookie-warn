fb9b1ffb8262f90da591aa310643309fc7e21b48
TS <git@schalk.hu>
2021-02-05 22:30:44 +0100

    Updated variables (sonarcloud issues)

--- gulpfile.js
+++ gulpfile.js
@@ -28,13 +28,13 @@ var banner = ['/**',
 gulp.task("js", function () {
 
 	var filename = "cookie-warn.js";
-	var package = require('./package.json');
+	var packageJson = require('./package.json');
 
 	return gulp.src([
 		SRC + "/*.js"
 	])		
 		.pipe(concat(filename))
-		.pipe(header(banner, { pkg: package }))
+		.pipe(header(banner, { pkg: packageJson }))
 		.pipe(gulp.dest(DIST));		
 
 });
@@ -44,7 +44,7 @@ gulp.task("js-min", function () {
 
 	var filename = "cookie-warn.js";
 
-	var package = require('./package.json');
+	var packageJson = require('./package.json');
 
 	return gulp.src([
 		DIST + "/" + filename,
@@ -52,7 +52,7 @@ gulp.task("js-min", function () {
 		.pipe(concat(filename))
 		.pipe(rename({ suffix: '.min' }))
 		.pipe(uglify())
-		.pipe(header(banner, { pkg: package }))
+		.pipe(header(banner, { pkg: packageJson }))
 		.pipe(gulp.dest(DIST))
 		.pipe(gzip({ append: true }))
 		.pipe(gulp.dest(DIST));
@@ -78,12 +78,12 @@ gulp.task('bump', function () {
 
 gulp.task('version', function () {
 
-	var package = require('./package.json');
+	var packageJson = require('./package.json');
 
 	return gulp.src([
 		SRC + '/cookie-warn.js',
 	])
-		.pipe(replace(/@version\s\d+\.\d+\.\d+/g, '@version ' + package.version))
+		.pipe(replace(/@version\s\d+\.\d+\.\d+/g, '@version ' + packageJson.version))
 		.pipe(gulp.dest(SRC));
 
 });
