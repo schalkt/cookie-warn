7ffd8d2e67a27f8114e7e19f0af97022ba4cab14
TS <git@schalk.hu>
2021-05-17 22:02:02 +0200

    Updated package.json and lock (vulnerabilities)

--- dist/cookie-warn.js
+++ dist/cookie-warn.js
@@ -1,7 +1,7 @@
 /**
  * @preserve cookie-warn - EU cookie warn
  * 
- * @version v3.1.18
+ * @version v3.1.19
  * @link https://schalk.hu/projects/cookie-warn/demo/index.html
  * @author Tamas Schalk (https://github.com/schalkt)
  * @license MIT
